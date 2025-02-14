653eaefa01b5b3b8bd6e895e818f69f2599a9177
Sidduzzzup <nova72949@gmail.com>
2025-02-14 08:27:48 +0530

    Updated authStore and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the build output goes to 'dist'
+    chunkSizeWarningLimit: 1000, // Prevents warning about large chunks
+  },
 })
