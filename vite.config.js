a05edc32500a705331a1137201dc7afb35fb366c
Siddartha <115263079+Sidduzzzup@users.noreply.github.com>
2025-02-22 20:14:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: './',  // 👈 Ensures relative paths in built files
-//   build: {
-//     outDir: 'dist',
-//     chunkSizeWarningLimit: 1000,
-//   },
-// });
-
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
