801220fbf68d55621dbbc8f45a3c9eb6eff27f05
Sidduzzzup <nova72949@gmail.com>
2025-02-22 23:35:37 +0530

    Added missing images

--- src/assets/components/Footersection.jsx
+++ src/assets/components/Footersection.jsx
@@ -10,7 +10,7 @@ const Footersection = () => {
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="#" class="flex items-center">
-                  <img src="/public/istockphoto-168362728-612x612.png" class="h-12 me-3" alt="FlowBite Logo" />
+                  <img src="/istockphoto-168362728-612x612.png" class="h-12 me-3" alt="FlowBite Logo" />
                   <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SENTENTIAL</span>
               </a>
           </div>
